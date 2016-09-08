ea1d9abc131bae692e6293e19348fe8d840a89a8
brenmiester <samuelsson.benjamin01@gmail.com>
2016-09-08 09:34:58 +0800

    Breed test
    Fixed check for breed. Still pretty buggy, but ok. Not done, but better

--- tmpmain.java
+++ tmpmain.java
@@ -2,8 +2,12 @@ import java.util.Scanner;
 
 public class run {
     public static void main(String[] args) {
+      	int health = 0;
+        int maxhealth = 0;
+        int rawattack = 0;
+        int attackrate = 0;
         Scanner userInputScanner = new Scanner(System.in);
-        System.out.print("What's your character's name? ");
+        System.out.println("What's your character's name?");
         String name = userInputScanner.nextLine();
         System.out.println(name + ", welcome to the world of Charjeng. This world is inhabited by 4 main classes of people...");
         System.out.println("The Mage");
@@ -20,37 +24,36 @@ public class run {
         System.out.println("So, " + name + ", who will you be?");
         System.out.println("Mage, Warrior, Archer or Monk?");
         String breed = userInputScanner.nextLine();
-        if (String breed = "Mage") {
+        if (breed.equals("Mage")) {
           System.out.println("Great choice. You are now a mage.");
-          int health = 20;
-          int maxhealth = 40;
-          int rawattack = 10;
-          int attackrate = 1;
+		    health = 20;
+            maxhealth = 40;
+            rawattack = 10;
+            attackrate = 1;
         }
-        if (String breed = "Warrior") {
+        if (breed.equals("Warrior")) {
           System.out.println("Great choice. You are now a Warrior.");
-          int health = 30;
-          int maxhealth = 60;
-          int rawattack = 15;
-          int attackrate = 2;
+          health = 30;
+          maxhealth = 60;
+          rawattack = 15;
+          attackrate = 2;
         }
-        if (String breed = "Archer") {
+        if (breed.equals("Archer")) {
           System.out.println("Great choice. You are now a Archer.");
-          int health = 25;
-          int maxhealth = 50;
-          int rawattack = 15;
-          int attackrate = 2;
+          health = 25;
+          maxhealth = 50;
+          rawattack = 15;
+          attackrate = 2;
         }
-        if (String breed = "Monk") {
+        if (breed.equals("Monk")) {
           System.out.println("Great choice. You are now a Monk.");
-          int health = 20;
-          int maxhealth = 40;
-          int rawattack = 20;
-          int attackrate = 2;
+          health = 20;
+          maxhealth = 40;
+          rawattack = 20;
+          attackrate = 2;
         }
         else {
             System.out.println("Error getting class. Check lines 23-44 or entry");
-            break;
         }
         System.out.println("Your current health is " + health);
         System.out.println("Your max health is " + maxhealth);
